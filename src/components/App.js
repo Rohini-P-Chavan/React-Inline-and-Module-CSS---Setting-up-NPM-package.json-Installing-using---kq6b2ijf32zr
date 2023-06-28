8017c1cfd8809136208438452cc32b61507b24f8
Rohini_P_Chavan <126341848+Rohini-P-Chavan@users.noreply.github.com>
2023-06-28 21:44:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import '../styles/App.css';
-import styles from '../styles/appStyles.module.css';
-const App = () => {
-const heading ={
 
-
+const data = {
+  '2018': [
+    'Avengers 1',
+    'Now you see me',
+    'Fast and Furious 7'
+  ],
+  '2019': [
+    'Avengers 2',
+    'Now you see me 2',
+    'Fast and Furious 8'
+  ],
+  '2020': [
+    'Final Avengers Movie(Iron man dies)',
+    'Now you finally used Lenskart',
+    'Covid Came'
+  ],
+  '2021': [
+    'Covid Returns',
+  ],
+  '2022': [
+    'Adventures of Saiman',
+    'Adventures of Shaktiman'
+  ]
 }
+const App = () => {
+
+  console.log(data);
+  const years = Object.keys(data);
+  console.log(years)
+
+  const [year, setYear] = useState('');
+  const [list, setList] = useState([]);
+
+  const selectYear = (e) => {
+    console.log('Year', e.target.value);
+    setYear(e.target.value);
+    const templist = data[e.target.value];
+    console.log(templist);
+    setList([...templist]);
+  }
+
+
   return (
     <div id="main">
-      <h1 >Success</h1>
-      <h1 id='error' >Error</h1>
+        <select onChange={selectYear}>
+
+          <option value={null}></option>
+          {years && years.map((item, index) => (
+            <option key={index+item} value={item}>{item}</option>
+          ))}
+
+        </select>
+        <br></br>
+
+        <div id="selected-year">{list.length > 0 ? 'Selected Year-' + year : 'No year selected'}</div>
+        <ul>
+        {
+          list && list.map((item, index) => (
+            <li key={index+item}>{item}</li>
+          ))
+        }
+        </ul>
     </div>
   )
 }
